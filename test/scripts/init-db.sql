2c96536e8227dc347f83529eb906f9e1704ebd27
blankclank <kheigenbarth@gmail.com>
2021-11-05 19:17:35 +0100

    feat: init-db.sql formatting

--- test/scripts/init-db.sql
+++ test/scripts/init-db.sql
@@ -1,59 +1,44 @@
-create schema queue;
-set search_path TO queue;
+CREATE SCHEMA queue;
 
-create type message_state as enum ('published', 'processed', 'processing_error');
 
-create table topics
-(
-    topic_id   uuid                    not null
-        constraint topics_pk
-            primary key,
-    topic_name varchar(128)            not null,
-    created_at timestamp default now() not null
-);
+SET search_path TO queue;
 
-create unique index topics_topic_id_uindex
-    on topics (topic_id);
 
-create table messages
-(
-    message_id      uuid                    not null
-        constraint message_pk
-            primary key,
-    topic_id        uuid                    not null,
-    created_at      timestamp default now() not null,
-    last_updated_at timestamp,
-    message_data    json                    not null
-);
+CREATE TYPE message_state AS enum ('published', 'processed', 'processing_error');
 
-create unique index message_message_id_uindex
-    on messages (message_id);
 
-create table subscriptions
-(
-    subscription_id uuid                    not null
-        constraint subscriptions_pk
-            primary key,
-    name            varchar(128)            not null,
-    topic_id        uuid                    not null
-        constraint subscriptions_topics_fk
-            references topics
-            on update restrict on delete restrict,
-    created_at      timestamp default now() not null
-);
+CREATE TABLE topics (topic_id UUID NOT NULL CONSTRAINT topics_pk PRIMARY KEY,
+                     topic_name varchar(128) NOT NULL,
+                     created_at TIMESTAMP DEFAULT now() NOT NULL);
 
-create unique index subscriptions_subscription_id_uindex
-    on subscriptions (subscription_id);
 
-create table subscriptions_messages
-(
-    id              uuid not null
-        constraint subscriptions_messages_pk
-            primary key,
-    subscription_id uuid not null,
-    message_id      uuid not null,
-    message_state   message_state default 'published'::message_state not null
-);
+CREATE UNIQUE INDEX topics_topic_id_uindex ON topics (topic_id);
 
-create unique index subscriptions_messages_id_uindex
-    on subscriptions_messages (id);
+
+CREATE TABLE messages (message_id UUID NOT NULL CONSTRAINT message_pk PRIMARY KEY,
+                       topic_id UUID NOT NULL,
+                       created_at TIMESTAMP DEFAULT now() NOT NULL,
+                       last_updated_at TIMESTAMP,
+                       message_data JSON NOT NULL);
+
+
+CREATE UNIQUE INDEX message_message_id_uindex ON messages (message_id);
+
+
+CREATE TABLE subscriptions
+(subscription_id UUID NOT NULL CONSTRAINT subscriptions_pk PRIMARY KEY,
+ name varchar(128) NOT NULL,
+ topic_id UUID NOT NULL CONSTRAINT subscriptions_topics_fk REFERENCES topics ON UPDATE RESTRICT ON DELETE RESTRICT,
+ created_at TIMESTAMP DEFAULT now() NOT NULL);
+
+
+CREATE UNIQUE INDEX subscriptions_subscription_id_uindex ON subscriptions (subscription_id);
+
+
+CREATE TABLE subscriptions_messages (id UUID NOT NULL CONSTRAINT subscriptions_messages_pk PRIMARY KEY,
+                                     subscription_id UUID NOT NULL,
+                                     message_id UUID NOT NULL,
+                                     message_state message_state DEFAULT 'published'::message_state NOT NULL);
+
+
+CREATE UNIQUE INDEX subscriptions_messages_id_uindex ON subscriptions_messages (id);
